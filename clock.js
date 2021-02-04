bbe80cf4fe887fa66a8d3c0bd480a3cc7d2c1855
yuri.vodich <yuri.vodich@gmail.com>
2021-02-04 05:04:26 +0300

    change class and prototype

--- clock.js
+++ clock.js
@@ -38,36 +38,20 @@ class Clock {
 
   class ExtendedClock  extends Clock {
     
-   date = undefined;
+    constructor(template) {
+      super(template)
+    }
 
-    render() {
-        let date = new Date();
-        
-        let hours = date.getHours();
-        if (hours < 10) hours = '0' + hours;
-    
-        let mins = date.getMinutes();
-        if (mins < 10) mins = '0' + mins;
-    
-        let secs = date.getSeconds();
-        if (secs < 10) secs = '0' + secs;
-
-        let precision  = this.date ? (date - this.date) : 1000;       
-        this.date = date;
-        
-        let output = this.template
-          .replace('h', hours)
-          .replace('m', mins)
-          .replace('s', secs)
-          .replace('ms', precision);    
-        console.log(output);
-      }
+    start(precision = 1000) {
+      this.render();
+      this.timer = setInterval(() => this.render(), precision);
+    }  
 
 }
 
-let extendedClock =  new ExtendedClock({template: 'h:m:s precision - ms'})
+let extendedClock =  new ExtendedClock({template: 'h:m:s'})
 
-extendedClock.start();
+extendedClock.start(2000);
 
 
 
